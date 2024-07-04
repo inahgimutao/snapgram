6d7d7ae6ea69220f3dcbb4285f93405df5f4a621
regina gimutao <gimutaoregina191@gmail.com>
2024-07-04 17:42:03 +0800

    fix bug

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -54,7 +54,7 @@ const PostForm = ({ post, action }: PostFormProps) => {
         imageUrl: post?.imageUrl,
       })
 
-      if(!updatePost) {
+      if(!updatedPost) {
         toast({
           title: 'Please try again',
         })
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -131,16 +131,16 @@ export const useUpdatePost = () => {
 
 export const useDeletePost = () => {
     const queryClient = useQueryClient();
-
     return useMutation({
-        mutationFn: ({postId, imageId}: {postId: string, imageId: string;}) => deletePost(postId, imageId),
-        onSuccess: (data) => {
-            queryClient.invalidateQueries({
-                queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
-            })
-        }
-    })
-}
+      mutationFn: ({ postId, imageId }: { postId?: string; imageId: string }) =>
+        deletePost(postId, imageId),
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        });
+      },
+    });
+  };
 
 export const useGetPosts = () => {
     return useInfiniteQuery({
