05a4b07463dae95c4d598117dcac9accd74636ae
regina gimutao <gimutaoregina191@gmail.com>
2024-07-04 16:24:20 +0800

    snapgram app

---
+++ src/components/shared/index.ts
@@ -0,0 +1,10 @@
+export { default as Home } from '../../_root/pages/Home';
+export { default as Explore } from '../../_root/pages/Explore';
+export { default as Saved } from '../../_root/pages/Saved';
+export { default as CreatePost } from '../../_root/pages/CreatePost';
+export { default as Profile } from '../../_root/pages/Profile';
+export { default as UpdateProfile } from '../../_root/pages/UpdateProfile';
+export { default as EditPost } from '../../_root/pages/EditPost';
+export { default as PostDetails } from '../../_root/pages/PostDetails';
+export { default as AllUsers } from '../../_root/pages/AllUsers';
+export { default as LikedPosts } from '../../_root/pages/LikedPosts';
